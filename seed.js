1f4df1f352460463c4f65ae2a1fff30dc7b809a7
Jake Chavez <jake94chavez@gmail.com>
2018-01-25 22:10:16 -0600

    prepares seed file

---
+++ seed.js
@@ -0,0 +1,196 @@
+var db = require('./models');
+
+var menteeList = [
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	},
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentor: 
+	}
+];
+
+var mentorList = [
+	{
+		name: '',
+		email: '',
+		age:
+		interests: [''],
+		desiredField: '',
+		mentee: 
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee: 
+	}
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee:
+	}
+	{
+		name: 
+		email: 
+		age:
+		interests:
+		desiredField:
+		mentee:
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee: 
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee:
+	},
+	{
+		name: 
+		email: 
+		age:
+		interests:
+		desiredField:
+		mentee:
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee: 
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee:
+	},
+	{
+		name:
+		email:
+		age:
+		interests:
+		desiredField:
+		mentee:
+	}
+];
+db.Mentee.remove({}, function(err, mentees){
+	if(err) {
+		console.log('Error occured in remove', err);
+	} else {
+		console.log('removed all mentees');
+
+		db.Mentee.create(menteeList, function(err, mentees){
+			if(err) { return console.log('ERROR', err); }
+			console.log('all mentees:', mentees);
+			console.log('created', mentees.length, 'mentees');
+			process.exit();
+		});
+	}
+});
+
+db.Mentor.remove({}, function(err, mentors){
+	if(err) {
+		console.log('Error occured in remove', err);
+	} else {
+		console.log('removed all mentors');mentor
+
+		db.Mentor.create(mentorList, function(err, mentors){
+			if(err) { return console.log('ERROR', err); }
+			console.log('all mentors:', mentors);
+			console.log('created', mentors.length, 'mentors');
+			process.exit();
+		});
+	}
+});
