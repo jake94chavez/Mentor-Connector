01725927c71343f853674040da93da3642b96770
Jake Chavez <jake94chavez@gmail.com>
2018-01-25 20:54:20 -0600

    sets up Schemas for Mentors and Mentees

---
+++ models/mentee.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MenteeSchema = new Schema ({
+	name: {
+		type: String,
+		required: [true, 'This user needs a Name']
+	},
+	email: {
+		type: String,
+		required: [true, 'This user needs an email']
+	},
+	age: Number,
+	interests: [String],
+	desiredField: String,
+	mentor: Mentor.Schema,
+});
+
+var Mentee = mongoose.model('Mentee', MenteeSchema)
+module.exports = Mentee;
+module.exports.Schema = MenteeSchema;
---
+++ models/mentor.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Mentee = require('./mentee')
+
+var MentorSchema = new Schema ({
+	name: {
+		type: String,
+		required: [true, 'This user needs a name']
+	},
+	email: {
+		type: String,
+		required: [true, 'This user needs an email']
+	},
+	age: {
+		type: Number
+	},
+	interests: [String],
+	field: String,
+	mentee: [Mentee.Schema],
+});
+
+var Mentor = mongoose.model('Mentor', MentorSchema);
+
+module.exports = Mentor;
+module.exports.Schema = MentorSchema;
